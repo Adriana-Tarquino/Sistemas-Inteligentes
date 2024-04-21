aa0118f724984f8abcd808bb9ea2241920c33fc4
Adriana Tarquino <atarquinocespedes@gmail.com>
2024-04-21 00:13:29 -0400

    Load file

--- Cubo Rubik/RubiksCube.py
+++ Cubo Rubik/RubiksCube.py
@@ -1,6 +1,20 @@
 
+def validate_cube(cube):
+    
+    return 
+
+
+
 class RubiksCube:
-    def __init__(self, value, color=None):
-       self.cube = value
+    def __init__(self, color):
        self.color = color
-       
+    
+    
+    def up(self):
+        return  
+    
+    def down(self):
+        return 
+    
+    def left(self, ):
+        return  
--- Cubo Rubik/main.py
+++ Cubo Rubik/main.py
@@ -1,34 +1,63 @@
 from RubiksCube import RubiksCube
+# strip() ->  Para elimnar espacios en blanco
+# split() -> para dividt la linea en una lista de elementos 
+#(si la linea esta vacia entonces quiere decir que termino una cara del cubo)
+
+def load_file(nombre_archivo):
+    cube = []
+    cara = []
+
+    with open(nombre_archivo, 'r') as archivo:
+        for linea in archivo:
+            fila = linea.strip().split()
+            if not fila:
+                cube.append(cara)
+                cara = []
+            else:
+                cara.append(fila)
+
+    cube.append(cara)  
+
+    return cube
 
 if __name__ == "__main__":
+   
+   archivo = 'Cubo Rubik\\rubik_cube_file.txt'
+
+   cube = load_file(archivo)
+
+   for cara in cube:
+      for fila in cara:
+         print(' '.join(fila))
+      print()  # Separador entre caras
     
-    cube = [ 
-            #white up
-             [ ['W','W','W'],
-               ['W','W','W'],
-               ['W','W','W']  ],
-            #green front
-             [ ['G','G','G'],
-               ['G','G','G'],
-               ['G','G','G']  ],
-            #red right
-            [  ['R','R','R'],
-               ['R','R','R'],
-               ['R','R','R']  ],
-            #blue back
-            [  ['B','B','B'],
-               ['B','B','B'],
-               ['B','B','B']  ],
-            #orange left
-            [  ['O','O','O'],
-               ['O','O','O'],
-               ['O','O','O']  ],
-            #yelow down
-            [  ['Y','Y','Y'],
-               ['Y','Y','Y'],
-               ['Y','Y','Y']  ],
+   #  cube = [ 
+   #          #white up
+   #           [ ['W','W','W'],
+   #             ['W','W','W'],
+   #             ['W','W','W']  ],
+   #          #green front
+   #           [ ['G','G','G'],
+   #             ['G','G','G'],
+   #             ['G','G','G']  ],
+   #          #red right
+   #          [  ['R','R','R'],
+   #             ['R','R','R'],
+   #             ['R','R','R']  ],
+   #          #blue back
+   #          [  ['B','B','B'],
+   #             ['B','B','B'],
+   #             ['B','B','B']  ],
+   #          #orange left
+   #          [  ['O','O','O'],
+   #             ['O','O','O'],
+   #             ['O','O','O']  ],
+   #          #yelow down
+   #          [  ['Y','Y','Y'],
+   #             ['Y','Y','Y'],
+   #             ['Y','Y','Y']  ],
             
-            ]
-    RubiksCube(cube)
+   #          ]
+   #  RubiksCube(cube)
     
     
